b63028bbc085b6b7fadc002e58cf2c45935ed874
Crudu Alexandra <alexandra.crudu@isa.utm.md>
2026-03-30 11:36:25 +0300

    error fix

--- astro-landing-page/astro.config.mjs
+++ astro-landing-page/astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 
-const repoName = "tum-web-lab2"; // must be a string
+const repoName = "tum-web-lab2";
+const isNetlify = process.env.NETLIFY === "true";
 
 export default defineConfig({
-  site: "https://crudualexandra.github.io",
-  base: `/${repoName}/`,
+  site: isNetlify
+    ? "https://cosmic-croquembouche-9a4e5b.netlify.app"
+    : "https://crudualexandra.github.io",
+  base: isNetlify ? "/" : `/${repoName}/`,
   output: "static",
 });
