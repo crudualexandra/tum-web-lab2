55be9ef5df8d2f97085a3a8d6c0dd3963c784a68
Crudu Alexandra <alexandra.crudu@isa.utm.md>
2026-03-30 11:39:43 +0300

    change url for netlify deploy

--- astro-landing-page/astro.config.mjs
+++ astro-landing-page/astro.config.mjs
@@ -5,7 +5,7 @@ const isNetlify = process.env.NETLIFY === "true";
 
 export default defineConfig({
   site: isNetlify
-    ? "https://cosmic-croquembouche-9a4e5b.netlify.app"
+    ? "https://magazintipografia.netlify.app"
     : "https://crudualexandra.github.io",
   base: isNetlify ? "/" : `/${repoName}/`,
   output: "static",
